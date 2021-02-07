0cd20a1a0fdf5eae7ddc2cf3b8b2275796438bb7
Moh Gumaa <gomaam14@gmail.com>
2021-02-07 20:06:50 +0400

    use state hook

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState("Mario");
+
   const handleClick = (name, e) => {
-    console.log(e);
-    console.log("hello " + name);
+    setName("Ali");
   };
 
   return (
     <div className="home">
-      <h2>Home</h2>
-      <button onClick={(e) => handleClick("Mohamed", e)}>Say My Name</button>
+      <h2>{name}</h2>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
