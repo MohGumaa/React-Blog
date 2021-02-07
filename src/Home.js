4075346677c9b625fa0c0165fe98d18fee645b71
Moh Gumaa <gomaam14@gmail.com>
2021-02-07 19:58:51 +0400

    Create event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+  const handleClick = (name, e) => {
+    console.log(e);
+    console.log("hello " + name);
+  };
+
   return (
     <div className="home">
       <h2>Home</h2>
+      <button onClick={(e) => handleClick("Mohamed", e)}>Say My Name</button>
     </div>
   );
 };
